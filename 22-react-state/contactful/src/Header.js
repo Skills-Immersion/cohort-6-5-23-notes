8fe280407bf9b6d870356073b5954c1471d46a5c
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-07-19 07:52:41 -0700

    state within one component

--- 22-react-state/contactful/src/Header.js
+++ 22-react-state/contactful/src/Header.js
@@ -1,14 +1,31 @@
 // import
-import React from 'react';
+import React, { useState } from 'react';
 
 // define the function that is the component
 function Header() {
+
+  // we can't just use standard JS variables - need to use a state variable so React knows to update the page
+  //let isLoggedIn = false;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  function logTheThing() {
+    console.log('the thing happened');
+  }
+  function clickHandler() {
+    console.log('clicked the button');
+    // isLoggedIn = true;
+    setIsLoggedIn(!isLoggedIn);
+  }
+
   return (
     <header className="App-header">
-      <h1 className="contacts-header">My Contacts</h1>
-      <h2>This is also part of the header</h2>
+      <h1 className="contacts-header" >
+        My Contacts
+        {isLoggedIn ? 'You are logged in!' : ''}
+        <button onClick={clickHandler}>{isLoggedIn ? 'Log Out' : 'Log In'}</button>
+      </h1>
+      <h2 onMouseEnter={logTheThing}>This is also part of the header</h2>
     </header>
-
   )
 }
 
