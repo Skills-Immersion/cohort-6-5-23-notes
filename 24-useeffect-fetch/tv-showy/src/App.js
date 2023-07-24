7252c0bf5840b685781a9c789c964aa26498d041
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-07-24 07:57:40 -0700

    fetch before the AbortController

--- 24-useeffect-fetch/tv-showy/src/App.js
+++ 24-useeffect-fetch/tv-showy/src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Show from './Show';
 
 function App() {
+  const [clicks, setClicks] = useState(0);
+  const [hovers, setHovers] = useState(0);
+  useEffect(() => {
+    document.title = `${Date.now()} is now`
+  }, [])
+
+  // setting up to make requests
+  // 1. a state variable to hold the data, eventually, that comes from the API
+  // 2. useEffect to make the request
+  // 3. fetch().then(json).then(set the state variable)
+  // 4. display data on the page
+
+  // 1
+  const [shows, setShows] = useState([]);
+  // 2
+  useEffect(() => {
+    // 3
+    // option A: promises with .then
+    // fetch("https://api.tvmaze.com/shows")
+    //   .then(response => response.json())
+    //   .then(data => setShows(data));
+    // option B: async/await
+    // write an async function, then call it
+    async function fetchAllTheShows() {
+      let response = await fetch("https://api.tvmaze.com/shows");
+      let data = await response.json();
+      setShows(data);
+    }
+    fetchAllTheShows();
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 onMouseOver={() => setHovers(hovers + 1)} onClick={() => setClicks(clicks + 1)}>TV Showy {hovers} {clicks}</h1>
+      {/* step 4 */}
+      {shows.map(s => <Show key={s.id} show={s} />)}
+      {/* {shows.length > 0 && <Show show={shows[0]} />} */}
     </div>
   );
 }
---
+++ 24-useeffect-fetch/tv-showy/src/Show.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Show({ show }) {
+  return <div className="card" style={{ width: '18rem' }}>
+    <h4>{show.name}</h4>
+    <img src={show.image.medium} />
+    <p>Premiered on {show.premiered} in {show.language}</p>
+  </div>
+}
+
+export default Show;
