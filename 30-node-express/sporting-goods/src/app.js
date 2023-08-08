a2864faf32b60cd747fffa9ffb047e6f196957af
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-08-08 08:09:00 -0700

    route level middleware

--- 30-node-express/sporting-goods/src/app.js
+++ 30-node-express/sporting-goods/src/app.js
@@ -32,26 +32,45 @@ const getCurrentTime = (req, res, next) => {
 
 app.get('/current-time', getCurrentTime)
 
+function validateItemExists(req, res, next) {
+  let { item } = req.query;
+  if (!item) {
+    next('must provide an item');
+  } else {
+    next();
+  }
+}
 
 // sportsball route - tells the client whether the included item is a sporting good
-app.get('/is-it-sport', (req, res, next) => {
-  console.log(req);
+app.get('/is-it-sport', validateItemExists, (req, res, next) => {
+  // console.log(req);
   console.log(req.query);
   const { item } = req.query;
-  if (!item) {
-    // go into error handling mode
-    next('item was undefined');
-  } else if (item.includes('sport') || item.includes('ball')) {
+  if (item.includes('sport') || item.includes('ball')) {
     res.send(`${item} is a sporting good`);
   } else {
     res.send('that is not a sporting good');
   }
 })
 
+// TODO:
+// Write a route that find the nearest available of an item.
+// The route should listen for a GET request to "/nearest".
+// The item should be a query parameter with a key of item.
+// The response should look like "The closest <item> is <n> miles away" where n is a random int between 1 and 10.
+
+// 'http://localhost:5000/nearest?item=soccer ball'
+app.get('/nearest', validateItemExists, (req, res, next) => {
+  let { item } = req.query;
+  res.send(`The closest ${item} is ${Math.floor(Math.random() * 10) + 1} miles away`);
+})
+
 app.get('/goods/my-orders', (req, res, next) => {
   res.send('You recently ordered a soccer ball and two goals');
 })
 
+// part of the URL with a :colon is a route parameter
+// 'http://localhost:5000/goods/soccer ball'
 app.get('/goods/:good', (req, res, next) => {
   console.log(req.params);
   res.send(`Yes, we have ${req.params.good} in stock!`)
@@ -64,7 +83,7 @@ app.get('/goods/:good', (req, res, next) => {
 //error handler
 app.use((error, req, res, next) => {
   console.log(error);
-  res.status(500).send("Sorry, something went wrong");
+  res.status(500).send(error.message ? error.message : error);
 })
 
 module.exports = app;
