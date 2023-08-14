5a0818ff46b1a6724e58ec694890a8cfd6c6a078
Michelle Ferreirae <mnfmnfm@gmail.com>
2023-08-14 07:59:41 -0700

    validations and updates for pokemon

--- 31-robust-server-structure/pokemon/src/pokemon/pokemon.controller.js
+++ 31-robust-server-structure/pokemon/src/pokemon/pokemon.controller.js
@@ -5,15 +5,61 @@ function list(req, res, next) {
   res.json({ data: pokemon })
 }
 
-function read(req, res, next) {
+function findPokemon(req, res, next) {
   const { id } = req.params;
   const index = pokemon.findIndex(p => p.id === id);
   if (index > -1) {
-    res.json({ data: pokemon[index] })
+    res.locals.index = index;
+    next();
   } else {
     next({
       status: 404,
       message: `no pokemon with id ${id}`
+    })
+  }
+}
+function read(req, res, next) {
+  const { index } = res.locals;
+  res.json({ data: pokemon[index] })
+}
+
+// TODO: validate our properties
+// req.body.data must exist
+// name, type, popularity should all exist
+// popularity should be a number between 1 and 10
+
+function validateBodyData(req, res, next) {
+  if (req.body.data) {
+    next();
+  } else {
+    next({
+      status: 400,
+      message: 'Request body must contain a key of data'
+    })
+  }
+}
+
+function validateProperties(req, res, next) {
+  let properties = ['name', 'type', 'popularity'];
+  for (let property of properties) {
+    if (!req.body.data[property]) {
+      return next({
+        status: 400,
+        message: `missing property ${property}`
+      })
+    }
+  }
+  next();
+}
+
+function validatePopularity(req, res, next) {
+  let { popularity } = req.body.data;
+  if (typeof popularity === 'number' && popularity >= 1 && popularity <= 10) {
+    next();
+  } else {
+    next({
+      status: 400,
+      message: "popularity must be a number between 1 and 10"
     })
   }
 }
@@ -35,8 +81,41 @@ function create(req, res, next) {
   res.status(201).json({ data: coolNewPokemon });
 }
 
+function update(req, res, next) {
+  let { index } = res.locals;
+  let { name, type, popularity } = req.body.data;
+  // adding the new data from the request into a new object to represent that pokemon
+  let newPokemonData = {
+    ...pokemon[index],
+    name,
+    type,
+    popularity
+  }
+  pokemon[index] = newPokemonData;
+  res.json({ data: newPokemonData })
+}
+
+function destroy(req, res, next) {
+  const { index } = res.locals;
+  pokemon.splice(index, 1);
+  res.status(204).send();
+}
+
 module.exports = {
   list,
-  read,
-  create
+  read: [findPokemon, read],
+  create: [
+    validateBodyData,
+    validateProperties,
+    validatePopularity,
+    create
+  ],
+  update: [
+    findPokemon,
+    validateBodyData,
+    validateProperties,
+    validatePopularity,
+    update
+  ],
+  destroy: [findPokemon, destroy]
 }
--- 31-robust-server-structure/pokemon/src/pokemon/pokemon.router.js
+++ 31-robust-server-structure/pokemon/src/pokemon/pokemon.router.js
@@ -9,5 +9,8 @@ router.route('/')
   .post(controller.create);
 
 router.route('/:id')
-  .get(controller.read);
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.destroy);
+
 module.exports = router;
