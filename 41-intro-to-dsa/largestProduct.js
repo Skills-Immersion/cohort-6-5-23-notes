20af3b3b4722a0fd36f71af53f98f424de302599
kelvin711 <arauzkelvin99@gmail.com>
2023-09-11 10:25:18 -0500

    intro to big O

---
+++ 41-intro-to-dsa/largestProduct.js
@@ -0,0 +1,17 @@
+// R - Repeat problem
+// E - Example input/output
+// A - Approach
+// C - Coding
+// T - Testing / Code review
+// O - Optimization / Follow-up questions
+
+
+// Given an array of numbers, find the largest product formed by two of the numbers. For example, 
+// if the input array is [1, 5, 3, 4, 2,], the largest product that can be formed by two of the numbers is 20, 
+// which is the product of 5 and 4. Can you write code to solve this problem?
+
+// will it have dupe numbers -> there can be
+// what happens if you get an empty array -> throw me an error check if there is only 2
+// will there be neg nums -> yes still give largest product
+
+
